70097357522e027dfd8fcf98714b857f972c22b5
carymrobbins <carymrobbins@gmail.com>
2013-07-23 22:04:24 -0500

    Added initial logic to main.

--- src/Game.hs
+++ src/Game.hs
@@ -9,7 +9,6 @@ import Control.Monad.Trans.State
 import Cards
 import Deck
 import Players
-import Helpers
 
 data Game = Game
     { _deck :: Deck
@@ -43,42 +42,4 @@ initHands = do
     dealCard dealer
     dealCard player
     dealCard dealer
-
-showRules :: InputString -> GoodOrBad OutputString
-showRules input = processResponse
-  where
-    response = map toLower input
-    processResponse
-        | response `elem` ["y", "yes", "yeah", "sure"] =
-            Good rules
-        | response `elem` ["n", "no", "nope", "nay"] =
-            Good "Ok, let's get started!\n"
-        | otherwise =
-            Bad "I'm sorry, I didn't understand.\n\
-                \Please say either yes or no.  "
-
-title :: String
-title = "\n\
-    \**************************\n\
-    \   Welcome to Hackjack!\n\
-    \**************************\n"
-
-rules :: String
-rules = "\n\
-    \Goal is to get as close to 21 without going over.\n\
-    \The player with the most points wins.\n\
-    \Cards 2-9 are worth their face value.\n\
-    \The 10 and face cards (J, Q, K) are worth 10 points.\n\
-    \Aces are worth 11 or 1, whichever is most favorable\n\
-    \to the player.\n"
-
-showTable :: Dealer -> Player -> TurnIsComplete -> String
-showTable d p t =
-    "Cash: $" ++ (show . getCash) p ++ "\n\
-    \n\
-    \Your hand:      " ++ (show . viewHand t) p ++ "\
-    \Dealer's hand:  " ++ (show . viewHand t) d ++ "\n\
-    \\n\
-    \You have " ++ (show . getPoints . viewHand t) p ++ ",\n\
-    \dealer is showing " ++ (show . getPoints . viewHand t) d ++ "\n"
 
--- src/Hackjack.hs
+++ src/Hackjack.hs
@@ -1,13 +1,15 @@
 module Main (main) where
 
+import Control.Lens
 import Control.Monad (liftM)
 import Control.Monad.Trans.State (execState)
+import Data.Char (toLower)
 import Text.Printf
 
 import Cards (getPoints, showHand)
 import Deck (newDeck)
-import Game (Game, dealer, newGame, player, roundInit, showRules, title)
-import Helpers (interactGoodOrBad)
+import Game (Game, _dealer, dealer, initHands, newGame, _player, player)
+import Helpers 
 import Players (TurnIsComplete, turnIncomplete, turnComplete, viewHand)
 
 main :: IO ()
@@ -21,22 +23,23 @@ intro = do
     interactGoodOrBad showRules
     putStrLn "Press ENTER to continue"
     getLine
-    game <- liftM (execState roundInit) newGame
-    putStrLn $ showGame game turnIncomplete
+    game <- newGame
+    let g = execState initHands game
+    putStrLn $ showGame g turnIncomplete
+    putStrLn "Press ENTER to continue"
+    getLine
     return ()
 
 showGame :: Game -> TurnIsComplete -> String
 showGame game turn =
     showGameBase
-        (showHand dealerHand)
+        (show dealerHand)
         (getPoints dealerHand)
-        (showHand playerHand)
+        (show playerHand)
         (getPoints playerHand)
   where
-    hand person = viewHand turn $ person game
-    --showPoints = show . getPoints
-    dealerHand = hand dealer
-    playerHand = hand player
+    dealerHand = game^.dealer.to (viewHand turn)
+    playerHand = game^.player.to (viewHand turn)
 
 showGameBase :: String -> Int -> String -> Int -> String
 showGameBase dealerHand dealerPoints playerHand playerPoints =
@@ -50,4 +53,31 @@ showGameBase dealerHand dealerPoints playerHand playerPoints =
         playerHand
         playerPoints
 
+title :: String
+title = "\n\
+    \**************************\n\
+    \   Welcome to Hackjack!\n\
+    \**************************\n"
+
+rules :: String
+rules = "\n\
+    \Goal is to get as close to 21 without going over.\n\
+    \The player with the most points wins.\n\
+    \Cards 2-9 are worth their face value.\n\
+    \The 10 and face cards (J, Q, K) are worth 10 points.\n\
+    \Aces are worth 11 or 1, whichever is most favorable\n\
+    \to the player.\n"
+
+showRules :: InputString -> GoodOrBad OutputString
+showRules input = processResponse
+  where
+    response = map toLower input
+    processResponse
+        | response `elem` ["y", "yes", "yeah", "sure"] =
+            Good rules
+        | response `elem` ["n", "no", "nope", "nay"] =
+            Good "Ok, let's get started!\n"
+        | otherwise =
+            Bad "I'm sorry, I didn't understand.\n\
+                \Please say either yes or no.  "
 
